65213bf32b71ad2d08c94da4c1656e40a1ef3db0
Pere <pere.solaclaver@gmail.com>
2019-09-25 14:37:38 +0100

    Create css variables inside mixins file

--- build-co-make/src/components/mixins.js
+++ build-co-make/src/components/mixins.js
@@ -1,8 +1,15 @@
 // CSS MIXINS THAT WILL BE REUSED ACROSS THE APP
 
-
 import React from "react";
 import styled, { css } from "styled-components";
+
+const fontSizeForm = css`
+    1.75rem;
+`;
+
+const buttonColor = css`
+    #52d5ac;
+`;
 
 const formMixin = css`
   display: flex;
@@ -12,14 +19,14 @@ const formMixin = css`
     display: flex;
     flex-flow: row wrap;
     align-items: center;
-    font-size: 1.75rem;
+    font-size: ${fontSizeForm};
     margin: 0.5rem;
     span {
       flex-grow: 1;
     }
     input,
     select {
-      font-size: 1.75rem;
+      font-size: ${fontSizeForm};
       padding: 0.75rem;
       margin-left: 1rem;
       flex-grow: 2;
@@ -35,7 +42,7 @@ const formMixin = css`
   textarea {
     margin-top: 0.5rem;
     width: 100%;
-    font-size: 1.75rem;
+    font-size: ${fontSizeForm};
     padding: 0.75rem;
   }
   .button {
@@ -44,11 +51,11 @@ const formMixin = css`
     justify-content: center;
     margin: 0.5rem;
     button {
-      font-size: 1.75rem;
+      font-size: ${fontSizeForm};
       border: none;
       border-radius: 5px;
       padding: 0.5rem;
-      background: #52d5ac;
+      background: ${buttonColor};
     }
   }
 `;
